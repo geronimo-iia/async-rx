ba4a13bd55fb4b6dddda44f8d94aa32f5d5b9f76
Jerome Guibert <jguibert@gmail.com>
2020-04-27 15:17:49 +0200

    finalize rx_subject_replay

--- tests/subject/test_replay_subject.py
+++ tests/subject/test_replay_subject.py
@@ -1,6 +1,15 @@
-from async_rx import Observer, rx_range, rx_subject_replay
+import pytest
+from async_rx import Observer, rx_range, rx_subject_replay, Subscription, rx_create
+from async_rx.protocol import default_subscription
 
 from ..model import ObserverCounterCollector
+
+
+def test_replay_subject_default():
+    with pytest.raises(RuntimeError):
+        rx_subject_replay(0)
+    with pytest.raises(RuntimeError):
+        rx_subject_replay(-1)
 
 
 def test_replay_subject(kernel):
@@ -23,6 +32,35 @@ def test_replay_subject(kernel):
     assert seeker_b.on_next_count == 4  # buffer size
     assert seeker_b.on_error_count == 0
     assert seeker_b.on_completed_count == 1
+
+    kernel.run(sub_a())
+    kernel.run(sub_b())
+
+
+def test_replay_subject_with_error(kernel):
+    async def _subscribe(an_observer: Observer) -> Subscription:
+        await an_observer.on_next("A")
+        await an_observer.on_error("Args")
+        return default_subscription
+
+    seeker_a = ObserverCounterCollector()
+    seeker_b = ObserverCounterCollector()
+    a_subject = rx_subject_replay(buffer_size=4)
+
+    sub_a = kernel.run(a_subject.subscribe(seeker_a))
+
+    sub_subject = kernel.run(rx_create(subscribe=_subscribe).subscribe(a_subject))
+
+    assert seeker_a.on_next_count == 1
+    assert seeker_a.on_error_count == 1
+    assert seeker_a.on_completed_count == 0
+    assert seeker_a.items == ["A"]
+
+    sub_b = kernel.run(a_subject.subscribe(seeker_b))
+    assert seeker_b.on_next_count == 1
+    assert seeker_b.on_error_count == 1
+    assert seeker_b.on_completed_count == 0
+    assert seeker_b.items == ["A"]
 
     kernel.run(sub_a())
     kernel.run(sub_b())
