ae375dd3bc30060ce343c9a2b05c0a015b163ddd
Jerome Guibert <jguibert@gmail.com>
2020-04-27 14:14:53 +0200

    finalize rx_amb

--- tests/observables/test_rx_amb.py
+++ tests/observables/test_rx_amb.py
@@ -1,7 +1,14 @@
-from async_rx import Observer, rx_amb, rx_create, rx_from, rx_range
+import curio
+import pytest
+from async_rx import Observer, rx_amb, rx_create, rx_from, rx_range, Subscription
 
 from ..model import ObserverCounterCollector
 from .model import countdown
+
+
+def test_rx_amb_init():
+    with pytest.raises(RuntimeError):
+        rx_amb()
 
 
 def test_rx_amb(kernel):
@@ -14,7 +21,7 @@ def test_rx_amb(kernel):
     obs = rx_amb(a, b)
     sub_a = kernel.run(obs.subscribe(seeker))
     kernel.run(sub_a())
-    print(seeker.items)
+
     assert seeker.on_completed_count == 1
     assert seeker.on_error_count == 0
     assert seeker.on_next_count == 1
@@ -31,8 +38,28 @@ def test_rx_amb2(kernel):
     obs = rx_amb(a, b)
     sub_a = kernel.run(obs.subscribe(seeker))
     kernel.run(sub_a())
-    print(seeker.items)
+
     assert seeker.on_completed_count == 1
     assert seeker.on_error_count == 0
     assert seeker.on_next_count == 1
     assert seeker.items == [b]
+
+
+def test_rx_amb_with_error(kernel):
+
+    seeker = ObserverCounterCollector()
+
+    async def _subscribe(an_observer: Observer) -> Subscription:
+        await curio.sleep(0.05)
+        await an_observer.on_error(err="Args")
+
+    a = rx_create(subscribe=kernel.run(countdown(5, 0.1)))
+
+    obs = rx_amb(a, rx_create(subscribe=_subscribe))
+    sub_a = kernel.run(obs.subscribe(seeker))
+    kernel.run(sub_a())
+
+    assert seeker.on_completed_count == 1
+    assert seeker.on_error_count == 0
+    assert seeker.on_next_count == 1
+    assert seeker.items == [a]
