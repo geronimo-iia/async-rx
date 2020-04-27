614d7920be2e62316935a981aafe7d4fc0585b5c
Jerome Guibert <jguibert@gmail.com>
2020-04-27 14:27:46 +0200

    finalize rx_avg

--- tests/observables/test_rx_avg.py
+++ tests/observables/test_rx_avg.py
@@ -1,4 +1,5 @@
-from async_rx import rx_avg
+from async_rx import rx_avg, Observer, Subscription, rx_create
+from async_rx.protocol import default_subscription
 
 from ..model import ObserverCounterCollector
 from .model import get_observable_to_21
@@ -14,3 +15,17 @@ def test_rx_avg(kernel):
     assert seeker.on_next_count == 1
     assert seeker.on_error_count == 0
     assert seeker.items == [10.0]
+
+
+def test_rx_avg_with_no_value_send_error(kernel):
+    async def _subscription(an_observer: Observer) -> Subscription:
+        await an_observer.on_completed()
+        return default_subscription
+
+    seeker = ObserverCounterCollector()
+    sub = kernel.run(rx_avg(observable=rx_create(subscribe=_subscription)).subscribe(seeker))
+    kernel.run(sub())
+
+    assert seeker.on_completed_count == 0
+    assert seeker.on_next_count == 0
+    assert seeker.on_error_count == 1
